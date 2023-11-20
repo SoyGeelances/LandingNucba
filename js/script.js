2e87acdc9c5239a9e028093fc2fd83910d527441
Genesis Sanchez <geelances@gmail.com>
2023-11-20 15:28:32 -0300

    updating the code

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+//menu burger
+/*document.addEventListener('DOMContentLoaded', function() {
+  const mobileMenu = document.getElementById('main-menu');
+  const nav = document.querySelector('.list-menu');
+
+  mobileMenu.addEventListener('click', function() {
+    nav.classList.toggle('show');
+  });
+});*/
